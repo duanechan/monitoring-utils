13357895eb5c06b4f15b48b7926bebfa594810d8
duanechan <2223483@slu.edu.ph>
2025-02-24 15:09:07 +0800

    Update email.go

--- credentials/internal/email.go
+++ credentials/internal/email.go
@@ -107,7 +107,7 @@ const (
             <tr>
               <td align="center" valign="middle" style="padding: 20px 20px;">
                 <a href="https://www.knowlesti.ph" target="_blank" style="display: inline-block;">
-                  <img src="https://i.imgur.com/SbZbhxG.jpeg" width="180" alt="Knowles Training Institute" style="border: 0; display: block;">
+                  <img src="https://knowlesti.sg/wp-content/uploads/2018/03/Knowles-Training-Institute-Logo.png.webp" width="180" alt="Knowles Training Institute" style="border: 0; display: block;">
                 </a>
               </td>
             </tr>
